e0e235158d4759344a8bc5d3ee2968828ef3a878
Code Crafter <162664649+itxHaroonKhan@users.noreply.github.com>
2025-06-30 06:53:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,56 +1,37 @@
+
 import os
 from dotenv import load_dotenv
-from agents import Agent, AsyncOpenAI, OpenAIChatCompletionsModel,RunConfig,Runner
-from pydantic import config
+from agents import Agent, AsyncOpenAI, RunConfig, Runner, OpenAIChatCompletionsModel
 
 
-
-# Load .env file
 load_dotenv()
 
-# Get API key from environment
-gemini_api_key = os.getenv("gemini_API_KEY")
+API = os.getenv("API_KEY")
 
 
-if not gemini_api_key:
-    raise ValueError("GEMINI_API_KEY is not set. Please ensure it is defined in your .env file.")
+client = AsyncOpenAI(api_key=API,base_url="https://generativelanguage.googleapis.com/v1beta/openai/")
 
-#Reference: https://ai.google.dev/gemini-api/docs/openai
-external_client = AsyncOpenAI(
-    api_key=gemini_api_key,
-    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
-)
 
 model = OpenAIChatCompletionsModel(
-    model="gemini-2.0-flash",
-    openai_client=external_client
+    model = "gemini-2.0-flash",
+     openai_client=client
 )
 
-config = RunConfig(
-    model=model,
-    model_provider=external_client,
-    tracing_disabled=True
-)
+config = RunConfig(model=model, model_provider=client, tracing_disabled=True)
 
 agent = Agent(
-    name="Translation Agent",
-    instructions="You are a helpful assistant that translates text from Urdu to English.",
-
+    name = "Gemini Agent",
+    instructions = "Your Name is Haroon and you are a helpful assistant."
 )
+
+
+say = input("How can I help you? ")
 
 response = Runner.run_sync(
     agent,
-    input="میں فل اسٹیک ڈویلپر بننے کے لیے پائتھون پروگرامنگ سیکھ رہا ہوں۔",
+    input= say,
     run_config=config
 )
 
 
-print(response)
-
-
-
-
-
-
-
-
+print(response.final_output)
